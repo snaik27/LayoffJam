372e11f6b67afb927781be2d6b098bdf72aaa7f1
Sid Naik <sid.naik27@gmail.com>
2024-01-26 19:13:34 -0800

    Scene, project structure. Audio manager prefab, music manager base code

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager _instance;
+    public AudioSource _audioSource;
+
+    [SerializeField] private AudioClip _openingTrack;
+    [SerializeField] private AudioClip _mainLoopTrack;
+    [SerializeField] private AudioClip _closingTrack;
+    
+    private void Awake()
+    {
+        _instance = this;
+        _audioSource.loop = true;
+    }
+    public void PlayOpeningTrack()
+    {
+        _audioSource.clip = _openingTrack;
+        _audioSource.Play();
+    }
+
+    public void PlayMainTrack()
+    {
+        _audioSource.clip = _mainLoopTrack;
+        _audioSource.Play();
+    }
+
+    public void PlayEndingTrack()
+    {
+        _audioSource.clip = _closingTrack;
+        _audioSource.Play();
+    }
+}
