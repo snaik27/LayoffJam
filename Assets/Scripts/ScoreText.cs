dfe95c144fc204d652c34ff781d7c91bb3c2e4da
Rocne Scribner <rocne@projectarcher.com>
2024-01-28 13:44:12 -0800

    changed scores to faceS

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -6,16 +6,24 @@ public class ScoreText : DialogueUI
     public void SetScore(int score)
     {
         Disposition disposition = ScoreManager.ScoreToDisposition(score);
-
-        string scoreText = score > 0 ? "+" : "";
-        scoreText += $"{score}";
-        SetText(scoreText);
-        _textToDisplay.color = disposition switch
+        switch (disposition)
         {
-            Disposition.Positive => Color.green,
-            Disposition.Negative => Color.red,
-            Disposition.Neutral => Color.yellow,
-            _ => Color.black
-        };
+            case Disposition.Positive:
+                _textToDisplay.color = Color.green;
+                _textToDisplay.SetText(":)");
+                break;
+            case Disposition.Negative:
+                _textToDisplay.color = Color.red;
+                _textToDisplay.SetText(":(");
+                break;
+
+            case Disposition.Neutral:
+                _textToDisplay.color = Color.yellow;
+                _textToDisplay.SetText(":|");
+                break;
+            default:
+                _textToDisplay.SetText("");
+                break;
+        }
     }
 }
