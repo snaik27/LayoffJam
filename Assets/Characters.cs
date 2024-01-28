fa3c66a950c055db75da0c959ae3c67f68e08a5d
Sid Naik <sid.naik27@gmail.com>
2024-01-28 11:34:48 -0800

    Characters.cs is character visual interface for MainLoopManager and GameStateManager

---
+++ Assets/Characters.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+/// <summary>
+/// Interface for MainLoopManager to tell characters what to do
+/// Set king, guest, jester from their respective objects in the main scene
+/// 
+/// </summary>
+public class Characters : MonoBehaviour
+{
+    [SerializeField] public Animator _king;
+    [SerializeField] public Animator _guest;
+    [SerializeField] public Animator _jester;
+
+    public static Characters _instance;
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void DoPhysicalJoke()
+    {
+        Debug.Log("IMPLEMENT VISUAL: Jester does physical joke");
+    }
+
+    public void DoVerbalJoke()
+    {
+        Debug.Log("IMPLEMENT VISUAL: Jester does visual joke");
+    }
+
+    public void DoGuestPositiveReaction()
+    {
+        Debug.Log("IMPLEMENT VISUAL: Guest positive reaction");
+    }
+
+    public void DoGuestNegativeReaction()
+    {
+        Debug.Log("IMPLEMENT VISUAL: King negative reaction");
+    }
+
+    public void DoKingPositiveReaction()
+    {
+        Debug.Log("IMPLEMENT VISUAL: King positive reaction");
+    }
+
+    public void DoKingNegativeReaction()
+    {
+        Debug.Log("IMPLEMENT VISUAL: King negative reaction");
+    }
+}
