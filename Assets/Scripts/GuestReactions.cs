2ece4b8e3fa21c51ba8e06fd8a7ca1fec7fcb731
Rocne Scribner <rocne.ks@gmail.com>
2024-01-27 17:22:14 -0800

    added some guest reactions (#13)
    * added some guest reactions
    
    * removed test/debug stuff
    
    * removed unused usings:

---
+++ Assets/Scripts/GuestReactions.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class GuestReactions : MonoBehaviour
+{
+    public TextAsset positive;
+    public TextAsset negative;
+    public TextAsset neutral;
+
+    private string[] _positiveReactions;
+    private string[] _negativeReactions;
+    private string[] _neutralReactions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _positiveReactions = positive.text.Split("\n");
+        _negativeReactions = negative.text.Split("\n");
+        _neutralReactions = neutral.text.Split("\n");
+    }
+
+    public string GetReaction(Guest.Disposition disposition)
+    {
+        return disposition switch
+        {
+            Guest.Disposition.Negative => GetNegativeReaction(),
+            Guest.Disposition.Positive => GetPositiveReaction(),
+            Guest.Disposition.Neutral => GetNeutralReaction(),
+            _ => throw new System.InvalidOperationException($"{disposition} is not a valid disposition"),
+        };
+    }
+
+    public string GetPositiveReaction()
+    {
+        return GetRandomString(_positiveReactions);
+    }
+    public string GetNegativeReaction()
+    {
+        return GetRandomString(_negativeReactions);
+    }
+
+    public string GetNeutralReaction()
+    {
+        return GetRandomString(_neutralReactions);
+    }
+
+    private string GetRandomString(string[] strings)
+    {
+        return strings[Random.Range(0, strings.Length)];
+    }
+}
